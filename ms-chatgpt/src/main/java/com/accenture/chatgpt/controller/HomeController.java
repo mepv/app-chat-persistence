31c8f4a7f1b9e95b5eea718c919f42f951d608ac
rafa2299 <rafa2299@gmail.com>
2023-05-05 17:07:21 -0300

    initial commit ms chat gpt

---
+++ ms-chatgpt/src/main/java/com/accenture/chatgpt/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.accenture.chatgpt.controller;
+
+
+import java.time.Duration;
+
+import com.accenture.chatgpt.model.Message;
+import com.accenture.chatgpt.model.Subscriber;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/message")
+    public Flux<Message> getAll() {
+        Flux<Message> lista = Flux.just(new Message("Hola")
+                , new Message("Hola 2")).delaySequence(Duration.ofSeconds(3));
+        return lista;
+    }
+    @GetMapping(path = "/numeros", produces = "text/event-stream")
+    public Flux<Integer> all () {
+        Flux<Integer> flux = Flux.range(1,30)
+                .delayElements(Duration.ofSeconds(1));
+
+        flux.subscribe(System.out::println); // suscriptor 1
+        flux.subscribe(Subscriber::multiplicar); // suscriptor 2
+        return flux; // retornamos el elemento. Sería como el suscriptor 3
+    }
+}
---
+++ ms-chatgpt/src/main/java/com/accenture/chatgpt/model/Subscriber.java
@@ -0,0 +1,7 @@
+package com.accenture.chatgpt.model;
+
+public class Subscriber {
+    public static void multiplicar(Integer n)  {
+        System.out.println("Subscriber2: "+n*n);
+    }
+}
